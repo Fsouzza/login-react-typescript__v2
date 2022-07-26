e8adcdbaf8fbaaa57ebd000a97f1f3591fd0bad2
F. Souzza <filipesouzza.adm@gmail.com>
2022-07-26 01:33:09 -0300

    correcoes estilizacao arquivos sass

--- src/pages/login/login.tsx
+++ src/pages/login/login.tsx
@@ -10,40 +10,40 @@ export const Login = () =>{
 
     return(
         <div className={styles.container}>
-        <div className={styles.container__login}>
-            <div className={styles.login__wrap}>
-            <form action="" className={styles.login__form}>
-                <span className={styles.login__form__title}>
-                    <img src={logo} alt="logo da empresa"/>
-                </span>
+            <div className={styles.container__login}>
+                <div className={styles.login__wrap}>
+                    <form action="" className={styles.login__form}>
+                        <span className={styles.login__form__title}>
+                            <img src={logo} alt="logo da empresa"/>
+                        </span>
 
-                <span className={styles.login__form__title}>Sign In</span>
-            
-                <div className={styles.wrap__input}>
-                    <input className={email !== "" ? `${styles.hasVal} ${styles.input}` : `${styles.input}`} type="email" value={email} onChange={e => setEmail(e.target.value)}/>
-                    <span className={styles.focus__input} data-placeholder='Email'></span>
-                </div>
+                        <span className={styles.login__form__title}>Sign In</span>
+                    
+                        <div className={styles.wrap__input}>
+                            <input className={email !== "" ? `${styles.hasVal} ${styles.input}` : `${styles.input}`} type="email" value={email} onChange={e => setEmail(e.target.value)}/>
+                            <span className={styles.focus__input} data-placeholder='Email'></span>
+                        </div>
 
-                <div className={`${styles.wrap__input} ${styles.wrap2}`}>   
-                    <input className={password !== "" ? `${styles.hasVal} ${styles.input}` : `${styles.input}`} type="password" value={password} onChange={e => setPassword(e.target.value)}/>
-                    <span className={styles.focus__input} data-placeholder='Password'></span>
-                </div>
+                        <div className={`${styles.wrap__input} ${styles.wrap2}`}>   
+                            <input className={password !== "" ? `${styles.hasVal} ${styles.input}` : `${styles.input}`} type="password" value={password} onChange={e => setPassword(e.target.value)}/>
+                            <span className={styles.focus__input} data-placeholder='Password'></span>
+                        </div>
 
-                <div className={styles.forgot}>
-                    <Link className={styles.txt3} to=''>Esqueceu sua senha?</Link>
-                </div>
+                        <div className={styles.forgot}>
+                            <Link className={styles.txt3} to=''>Esqueceu sua senha?</Link>
+                        </div>
 
-                <div className={styles.containerLogin__btn}>
-                    <Botao>
-                        Login
-                    </Botao>
-                </div>
+                        <div className={styles.containerLogin__btn}>
+                            <Botao>
+                                Login
+                            </Botao>
+                        </div>
 
-                <div className='text-center'>
-                    <span className={styles.txt1}>Não possui conta?</span>
-                    <Link className={styles.txt2} to='/register'>Criar conta.</Link>
-                </div>
-            </form>
+                        <div className={styles.text__center}>
+                            <span className={styles.txt1}>Não possui conta?</span>
+                            <Link className={styles.txt2} to='/register'>Criar conta.</Link>
+                        </div>
+                    </form>
             </div>
         </div>  
     </div> 
--- src/pages/register/register.tsx
+++ src/pages/register/register.tsx
@@ -1,8 +1,10 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
-import Botao from "../../components/botao";
+import Botao from "components/botao";
 import {validateEmail, validateNome, validatePassword} from '../../utils/regex';
-import './register.css'
+import styles from './register.module.scss';
+import styles2 from 'pages/login/login.module.scss';
+
 
 export const Register = () => {
     const logo = require('../../assets/img/foxLogo.png')
@@ -40,47 +42,47 @@ export const Register = () => {
     
 
     return(
-        <div className="container">
-        <div className="container-login">
-            <div className="wrap-login">
-                <form className="login-form">
-                    <span className="login-form-title">
-                        <img src={logo} alt="logo da empresa"/>
-                    </span>
-                    <span className="login-form-title">Sign Up</span>
-                    
-                    <div className='formInputs'>
-                        <label htmlFor="name">Nome Completo:</label>
-                        <input className={nameErr === true ? 'inputError inputForm' : 'inputForm'} name="name" type="text" value={name} onChange={e => setRegisterData({name: e.target.value, email, password, repeatPassword})} required/>
-                        {nameErr && <span className="erroRegex">Digite seu Nome e Sobrenome</span>}
-                    </div>
+        <div className={styles2.container}>
+            <div className={styles2.container__login}>
+                <div className={styles2.login__wrap}>
+                <form action="" className={styles2.login__form}>
+                        <span className={styles2.login__form__title}>
+                            <img src={logo} alt="logo da empresa"/>
+                        </span>
+                        <span className={styles2.login__form__title}>Sign Up</span>
+                        
+                        <div className={styles.formInputs}>
+                            <label htmlFor="name">Nome Completo:</label>
+                            <input className={nameErr === true ? `${styles.inputError} ${styles.inputForm}` : `${styles.inputForm}`} name="name" type="text" value={name} onChange={e => setRegisterData({name: e.target.value, email, password, repeatPassword})} required/>
+                            {nameErr && <span className={styles.erroRegex}>Digite seu Nome e Sobrenome</span>}
+                        </div>
 
-                    <div className='formInputs'>
-                        <label htmlFor="email">E-mail:</label>
-                        <input className={emailErr === true ? 'inputError inputForm' : 'inputForm'} name="email" type="email" value={email} onChange={e => setRegisterData({name, email: e.target.value, password, repeatPassword})} required/>
-                        {emailErr && <span className="erroRegex">Digite um e-mail válido</span>}
-                    </div>
-                    
+                        <div className={styles.formInputs}>
+                            <label htmlFor="email">E-mail:</label>
+                            <input className={emailErr === true ? `${styles.inputError} ${styles.inputForm}` : `${styles.inputForm}`} name="email" type="email" value={email} onChange={e => setRegisterData({name, email: e.target.value, password, repeatPassword})} required/>
+                            {emailErr && <span className={styles.erroRegex}>Digite um e-mail válido</span>}
+                        </div>
+                        
 
-                    <div className='formInputs'>
-                        <label htmlFor="password">Senha:</label>
-                        <input className={passwordErr === true ? 'inputError inputForm' : 'inputForm'} name="password" type="password" value={password} onChange={e => setRegisterData({name, email, password: e.target.value, repeatPassword})} required/>
-                        {passwordErr && <span className="erroRegex">Senha deve conter ao menos uma letra maiúscula e caractere especial</span>}
-                    </div>
+                        <div className={styles.formInputs}>
+                            <label htmlFor="password">Senha:</label>
+                            <input className={passwordErr === true ? `${styles.inputError} ${styles.inputForm}` : `${styles.inputForm}`} name="password" type="password" value={password} onChange={e => setRegisterData({name, email, password: e.target.value, repeatPassword})} required/>
+                            {passwordErr && <span className={styles.erroRegex}>Senha deve conter ao menos uma letra maiúscula e caractere especial</span>}
+                        </div>
 
-                    <div className='container-login-form-btn'>
-                        <Botao onClick={validate}>
-                            Cadastrar
-                        </Botao>
-                    </div>
+                        <div className={styles2.containerLogin__btn}>
+                            <Botao onClick={validate}>
+                                Cadastrar
+                            </Botao>
+                        </div>
 
-                    <div className='text-center'>
-                        <span className='txt1'>Já possui possui um cadastro?</span>
-                        <Link className='txt2' to='/'>Acesse.</Link>
-                    </div>
-                </form>
-            </div>
-        </div>  
-    </div> 
+                        <div className={styles2.text__center}>
+                            <span className={styles2.txt1}>Já possui possui um cadastro?</span>
+                            <Link className={styles2.txt2} to='/'>Acesse.</Link>
+                        </div>
+                    </form>
+                </div>
+            </div>  
+        </div> 
     );
 }
